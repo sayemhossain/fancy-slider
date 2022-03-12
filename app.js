bac93da8341f150f51409d9697b2f1c3305f5d9e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-12 20:26:11 +0600

    slider problem solve

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ const createSlider = () => {
   document.querySelector(".main").style.display = "block";
   // hide image aria
   imagesArea.style.display = "none";
-  const duration = document.getElementById("duration").value || 1000;
+  const duration = document.getElementById("sliders").value || 1000;
   sliders.forEach((slide) => {
     let item = document.createElement("div");
     item.className = "slider-item";
